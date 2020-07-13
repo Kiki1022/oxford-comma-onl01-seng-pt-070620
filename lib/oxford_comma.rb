7ef3da4800520404fa3e278b10c9cfadef523eb6
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-13 03:29:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ end
 if array.length == 2
   return array.join(" and ")
 end
-if array.length == 3
+if array.length >= 3 
    element = array.pop
    array << ", and #[element]"
    return array.join(", ")
