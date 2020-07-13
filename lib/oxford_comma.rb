ec02271d6ba70d5a971fc937ac8ee5dccb891669
Kinnari Shah <kinnari1022@yahoo.com>
2020-07-13 03:19:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,11 +9,11 @@ if array.length == 2
   return array.join(" and ")
 end
 if array.length == 3
-   
+    element = array[-1]
    return array.join(", ")
   end
    
-   element = array[-1]
+  
      
    #return
    
